a5b8b7d241db01520ae7f0d7238f1652f142facc
B0nBun <rgusiakov@gmail.com>
2023-02-16 08:52:50 +0300

    refactor(Vehicles): narrowed the `Vehicles` class api to more general methods

--- src/lib/Vehicles.java
+++ src/lib/Vehicles.java
@@ -3,7 +3,9 @@ package lib;
 import java.time.LocalDate;
 import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 import lib.entities.Coordinates;
 import lib.entities.FuelType;
@@ -47,76 +49,30 @@ public class Vehicles {
         this.list = new LinkedList<>();
     }
     
-    public LinkedList<Vehicle> getAll() {
-        return this.list;
+    public Stream<Vehicle> stream() {
+        return this.list.stream();
     }
 
-    public Vehicle head() {
-        return this.list.getFirst();
-    }
-
-    public Vehicle findMin(Comparator<Vehicle> comparator) {
-        Vehicle minVehicle = null;
-        for (Vehicle vehicle : this.list) {
-            if (minVehicle == null || comparator.compare(vehicle, minVehicle) < 0) {
-                minVehicle = vehicle;
-            }
-        }
-        return minVehicle;
-    }
-
-    public Float enginePowerAverage() {
-        Float sum = 0f;
-        for (int i = 0; i < this.list.size(); i ++) {
-            sum += this.list.get(i).enginePower();
-        }
-        return sum / this.list.size();
-    }
-    
     public void add(VehicleCreationSchema newVehicle) {
         this.idCounter ++;
         this.list.add(newVehicle.generate(this.idCounter, LocalDate.now()));
     }
 
-    public boolean addIfMin(VehicleCreationSchema newVehicle) {
-        Vehicle minVehicle = this.findMin(Vehicle::compareTo);
-
-        this.idCounter ++;
-        Vehicle createdVehicle = newVehicle.generate(this.idCounter, LocalDate.now());
-        int comparison = minVehicle.compareTo(createdVehicle);
-
-        if (comparison > 0) {
-            this.list.add(createdVehicle);
-            return true;
-        }
-        this.idCounter --;
-        return false;
-    }
-    
-    public boolean update(long id, VehicleCreationSchema updatedSchema) {
-        boolean listChanged = false;
+    public Vehicles mutate(Function<Vehicle, Vehicle> mutator) {
         for (int i = 0; i < this.list.size(); i ++) {
-            Vehicle foundVehicle = this.list.get(i);
-            if (foundVehicle.id() == id) {
-                listChanged = true;
-                this.list.set(i, updatedSchema.generate(
-                    foundVehicle.id(),
-                    foundVehicle.creationDate()
-                ));
-            }
+            var mutated = mutator.apply(this.list.get(i));
+            this.list.set(i, mutated);
         }
-        return listChanged;
+        return this;
     }
 
-    public boolean removeIf(Predicate<Vehicle> predicate) {
-        return this.list.removeIf(predicate);
+    public Vehicles removeIf(Predicate<Vehicle> predicate) {
+        this.list.removeIf(predicate);
+        return this;
     }
 
-    public void clear() {
+    public Vehicles clear() {
         this.list.clear();
-    }
-    
-    public String serialize() {
-        return "TODO";
+        return this;
     }
 }
