81fa2d3c88c1cac8e846f363dd5d75c99fd02213
B0nBun <rgusiakov@gmail.com>
2023-02-25 18:15:46 +0300

    test: added 'resources/testing.xml' file

--- src/main/java/ru/ifmo/app/Test.java
+++ src/main/java/ru/ifmo/app/Test.java
@@ -1,7 +1,7 @@
 package ru.ifmo.app;
 
 import java.io.IOException;
-import java.io.StringReader;
+import java.net.URL;
 
 import javax.xml.XMLConstants;
 
@@ -28,32 +28,20 @@ class Foo {
     }
 }
 
-public class Test {
-    private static String xmlString = """
-    <root>
-        <elem tip=\"someattr1\">
-            <optional>content1</optional>
-        </elem>
-        <elem tip=\"someattr2\">content2</elem>
-        <elem tip=\"someattr3\">content3</elem>
-    </root>
-    """;
-    
+public class Test {    
     public static void main(String[] args) throws IOException, JDOMException {
+        URL testingFile = Test.class.getClassLoader().getResource("testing.xml");
+        System.out.println(testingFile);
         var sax = new SAXBuilder();
         
         sax.setProperty(XMLConstants.ACCESS_EXTERNAL_DTD, "");
         sax.setProperty(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
 
-        Document doc = sax.build(new StringReader(Test.xmlString));
+        Document doc = sax.build(testingFile);
         Element rootNode = doc.getRootElement();
         var list = rootNode.getChildren();
         for (var e: list) {
             System.out.println(e);
-            Element child = e.getChild("optional");
-            if (child != null) {
-                System.out.println("Child: " + child.getText());
-            }
         }
     }
 }
