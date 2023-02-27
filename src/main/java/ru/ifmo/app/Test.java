1c48c7d7daa1562f95db165d69970ddaf991ff77
B0nBun <rgusiakov@gmail.com>
2023-02-27 21:16:04 +0300

    fix(logback.xml): fixed STDOUT appender, so it logs on every level except 'trace' and 'debug'

--- src/main/java/ru/ifmo/app/Test.java
+++ src/main/java/ru/ifmo/app/Test.java
@@ -16,6 +16,7 @@ import ru.ifmo.app.lib.exceptions.ParsingException;
 public class Test {    
     public static void main(String[] args) throws IOException, JDOMException, ParsingException {
         Logger logger = LoggerFactory.getLogger("ru.ifmo.app.logger");
+        logger.trace("trace message");
         logger.debug("debug message");
         logger.info("info message");
         logger.warn("warn message");
