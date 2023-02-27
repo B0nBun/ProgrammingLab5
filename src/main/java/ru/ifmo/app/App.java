6d7776cebc93fa03a37e5966135dab8b5d8088a4
B0nBun <rgusiakov@gmail.com>
2023-02-27 18:57:27 +0300

    feat: starting with empty collection if any error during file reading is thrown, instead of quitting the program

--- src/main/java/ru/ifmo/app/App.java
+++ src/main/java/ru/ifmo/app/App.java
@@ -2,6 +2,7 @@ package ru.ifmo.app;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.NoSuchElementException;
@@ -30,25 +31,31 @@ public class App {
 		}
 
 		try (
-			var vehiclesXmlFileStream = vehiclesXmlFile != null ? new FileInputStream(vehiclesXmlFile) : null;
 			var scanner = new Scanner(System.in);
 			var outputWriter = new PrintWriter(System.out);
 		) {
 
 			Vehicles vehicles = null;
-			try {
+			try (
+				var vehiclesXmlFileStream = vehiclesXmlFile != null ? new FileInputStream(vehiclesXmlFile) : null;
+			) {		
 				if (vehiclesXmlFileStream != null) {
 					vehicles = Vehicles.loadFromXml(vehiclesXmlFileStream, outputWriter);
 					Utils.print(outputWriter, "Loaded " + vehicles.stream().count() + " elements from provided file\n");
 				} else {
-					Utils.print(outputWriter, "No xml files in arguments were provided, starting with empty collection..." + "\n");
-					vehicles = new Vehicles();
+					Utils.print(outputWriter, "No xml files in arguments were provided\n");
 				}
 			} catch (JDOMException err) {
 				Utils.print(outputWriter, "Couldn't parse xml file '" + vehiclesXmlFile + "': " + err.getMessage() + "\n");
-				return;
+			} catch (FileNotFoundException err) {
+				Utils.print(outputWriter, "File '" + vehiclesXmlFile + "' not found: " + err.getMessage() + "\n");
 			}
-	
+
+			if (vehicles == null) {
+				Utils.print(outputWriter, "Starting with empty collection...\n");
+				vehicles = new Vehicles();
+			}
+		
 			var executor = new CommandExecutor(scanner, outputWriter, vehicles, vehiclesXmlFile);
 	
 			try {
@@ -67,7 +74,7 @@ public class App {
 				Utils.print(outputWriter, "Illegal state exception: " + err.getMessage() + "\n");
 			}
 		} catch (IOException err) {
-			System.out.println("IOException occured: " + err + "\n");
+			System.out.println("IOException occured: " + err.getMessage() + "\n");
 		}
 	}
 }
