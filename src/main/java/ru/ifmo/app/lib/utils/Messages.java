81a82a1d540d32f444394eb9191db4f91663069d
B0nBun <rgusiakov@gmail.com>
2023-02-28 21:10:27 +0300

    feat: If the message is not found in 'Messages' resource bundle, than error is not thrown and the key is returned

--- src/main/java/ru/ifmo/app/lib/utils/Messages.java
+++ src/main/java/ru/ifmo/app/lib/utils/Messages.java
@@ -1,12 +1,18 @@
 package ru.ifmo.app.lib.utils;
 
 import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Messages {
     private static final ResourceBundle bundle = ResourceBundle.getBundle("ApplicationMessages");
 
     public static String get(String key, Object... args) {
-        return MessageFormat.format(Messages.bundle.getString(key), args);
+        try {
+            return MessageFormat.format(Messages.bundle.getString(key), args);
+        } catch (MissingResourceException | IllegalArgumentException err) {
+            return key + Arrays.asList(args);
+        }
     }
 }
