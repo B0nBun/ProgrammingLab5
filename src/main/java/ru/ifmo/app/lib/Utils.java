558d3dc0c3a417c90288025be53c810307f2c9f2
B0nBun <rgusiakov@gmail.com>
2023-03-25 21:43:08 +0300

    refactor(Utils): removed unused method `scanUntilValid`

--- src/main/java/ru/ifmo/app/lib/Utils.java
+++ src/main/java/ru/ifmo/app/lib/Utils.java
@@ -3,10 +3,8 @@ package ru.ifmo.app.lib;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Optional;
-import java.util.Scanner;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import ru.ifmo.app.App;
 import ru.ifmo.app.lib.exceptions.ParsingException;
 import ru.ifmo.app.lib.exceptions.ValidationException;
 import ru.ifmo.app.lib.utils.Messages;
@@ -44,54 +42,6 @@ public class Utils {
     }
 
     return path;
-  }
-
-  /**
-   * Using the passed scanner, starts a loop which prmopts the user, scans the next line and breaks
-   * the loop only if the provided input is parsed without exceptions and passes the validator.
-   *
-   * @param <T> A type of the value, which should be 'gotten' from the scanned input
-   * @param parsingFunction A function that accepts a String and returns a value of a generic type
-   *        {@code T} (can throw a ParsingException to signify that the parsing failed)
-   * @param validator A function that accepts a value of type {@code T} and returns an
-   *        {@link Optional} of a String. See functional interface {@link DeprecatedValidator} for
-   *        more detailed description
-   * @param scanner A Scanner which is used to get the input by calling the
-   *        {@link Scanner#nextLine()}
-   * @param inputString A String which is logged before the prompt
-   * @param parsingErrorMessage A function which converts a {@link ParsingException} thrown by the
-   *        {@code parsingFunction} to a string message
-   * @return A value of type {@code T} scanned from the line in the Scanner. This value is
-   *         guaranteed to have passed the parsing and validation processes
-   */
-  public static <T> T scanUntilValid(ParsingFunction<T> parsingFunction,
-      DeprecatedValidator<T> validator, Scanner scanner, String inputString,
-      Function<ParsingException, String> parsingErrorMessage, boolean logScanned) {
-    while (true) {
-      App.logger.info(inputString);
-      String line = scanner.nextLine().trim();
-      if (logScanned) {
-        App.logger.info(line);
-      }
-
-      if (line.length() == 0) {
-        var validationError = validator.validate(null);
-        if (validationError.isEmpty())
-          return null;
-        App.logger.error(validationError.get());
-        continue;
-      }
-
-      try {
-        var result = parsingFunction.tryToParse(line);
-        var validationError = validator.validate(result);
-        if (validationError.isEmpty())
-          return result;
-        App.logger.error(validationError.get());
-      } catch (ParsingException exception) {
-        App.logger.error(Messages.get("Error.Parsing", parsingErrorMessage.apply(exception)));
-      }
-    }
   }
 
   public static Optional<String> validateFilename(String filename) {
