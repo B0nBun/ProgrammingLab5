c00e32310092aa4d59676aca610a6cadfb483c6b
B0nBun <rgusiakov@gmail.com>
2023-04-08 12:15:53 +0300

    refactor(Server): decomposed the 'main' method on the server side

--- src/main/java/ru/ifmo/app/server/Server.java
+++ src/main/java/ru/ifmo/app/server/Server.java
@@ -3,7 +3,9 @@ package ru.ifmo.app.server;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.nio.ByteBuffer;
@@ -17,6 +19,49 @@ import ru.ifmo.app.server.exceptions.InvalidCommandParametersException;
 
 // TODO: Ctrl+C on client causes BufferUnderflowException
 public class Server {
+  private static ClientMessage<Object> getClientMessageFromStream(InputStream in)
+      throws IOException, ClassNotFoundException {
+    byte[] sizeBytes = in.readNBytes(Integer.BYTES);
+    int objectSize = ByteBuffer.wrap(sizeBytes).getInt();
+    byte[] objectBytes = in.readNBytes(objectSize);
+    var bytesInput = new ByteArrayInputStream(objectBytes);
+    var objectInput = new ObjectInputStream(bytesInput);
+    var message = ClientMessage.uncheckedCast(objectInput.readObject());
+    bytesInput.close();
+    objectInput.close();
+    return message;
+  }
+
+  private static boolean handleClient(InputStream in, OutputStream out, CommandExecutor executor)
+      throws IOException, ClassNotFoundException {
+
+    var message = Server.getClientMessageFromStream(in);
+
+    try (var byteOutput = new ByteArrayOutputStream(); var writer = new PrintWriter(byteOutput)) {
+      try {
+        executor.execute(message, writer);
+      } catch (InvalidCommandParametersException err) {
+        writer.println("Invalid parameter object was passed to the command: " + err);
+      } catch (ExitProgramException err) {
+        writer.println("Exiting");
+        return true;
+      }
+
+      var error = writer.checkError();
+      if (error) {
+        System.out.println("Error in print writer occured");
+      }
+
+      writer.flush();
+      var response =
+          new ServerResponse(new String(byteOutput.toByteArray(), StandardCharsets.UTF_8));
+      var responseBuffer = Utils.objectToBuffer(response);
+      out.write(responseBuffer.array());
+    }
+
+    return false;
+  }
+
   public static void main(String[] args) throws IOException, ClassNotFoundException {
     int port = 1111;
     try (var server = new ServerSocket(port);) {
@@ -30,38 +75,10 @@ public class Server {
           Vehicles vehicles = new Vehicles();
           var executor = new CommandExecutor(vehicles);
 
-          boolean clientQuit = false;
-          while (!clientQuit) {
-            byte[] sizeBytes = in.readNBytes(Integer.BYTES);
-            int objectSize = ByteBuffer.wrap(sizeBytes).getInt();
-            byte[] objectBytes = in.readNBytes(objectSize);
-            var bytesInput = new ByteArrayInputStream(objectBytes);
-            var objectInput = new ObjectInputStream(bytesInput);
-            var message = ClientMessage.uncheckedCast(objectInput.readObject());
-            bytesInput.close();
-            objectInput.close();
-
-            try (var byteOutput = new ByteArrayOutputStream();
-                var writer = new PrintWriter(byteOutput)) {
-              try {
-                executor.execute(message, writer);
-              } catch (InvalidCommandParametersException err) {
-                writer.println("Invalid parameter object was passed to the command: " + err);
-              } catch (ExitProgramException err) {
-                writer.println("Exiting...");
-                clientQuit = true;
-              }
-
-              var error = writer.checkError();
-              if (error) {
-                System.out.println("Error in print writer occured:");
-              }
-              writer.flush();
-              var response =
-                  new ServerResponse(new String(byteOutput.toByteArray(), StandardCharsets.UTF_8));
-              var responseBuffer = Utils.objectToBuffer(response);
-              out.write(responseBuffer.array());
-            }
+          while (true) {
+            boolean clientQuit = Server.handleClient(in, out, executor);
+            if (clientQuit)
+              break;
           }
         }
       }
